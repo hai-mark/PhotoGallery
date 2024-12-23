6d5f47e18cd1625905f8ddda2fa53f8eafcd5589
hai-mark <errrorfour@gmail.com>
2024-12-24 06:11:11 +0700

    Связь ThumbnailDownloader с жизненным  циклом (ThumbnailDownloader.kt)

--- app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.kt
@@ -2,7 +2,9 @@ package com.bignerdranch.android.photogallery
 
 import android.os.HandlerThread
 import android.util.Log
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 
 private const val TAG = "ThumbnailDownloader"
 class ThumbnailDownloader<in T> : HandlerThread(TAG), LifecycleObserver {
@@ -10,6 +12,19 @@ class ThumbnailDownloader<in T> : HandlerThread(TAG), LifecycleObserver {
     override fun quit(): Boolean {
         hasQuit = true
         return super.quit()
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun setup() {
+        Log.i(TAG, "Starting background thread")
+        start()
+        looper
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun tearDown() {
+        Log.i(TAG, "Destroying background thread")
+        quit()
     }
 
     fun queueThumbnail(target: T, url: String)
