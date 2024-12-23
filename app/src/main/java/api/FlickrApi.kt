84b1f48b6659dc190b80de469879d34a20272f6a
hai-mark <errrorfour@gmail.com>
2024-12-24 05:24:24 +0700

    Определение запроса «получить недавние  интересные фотографии» (api/FlickrApi.kt)

--- app/src/main/java/api/FlickrApi.kt
+++ app/src/main/java/api/FlickrApi.kt
@@ -5,6 +5,12 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface FlickrApi {
-    @GET("/")
-    fun fetchContents(): Call<String>
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=9f262b8cda16e306c535932c0fca8bbe" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<String>
 }
