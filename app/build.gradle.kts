35946de60c8abcba8b528a045ad1f16b4af98d21
hai-mark <errrorfour@gmail.com>
2024-12-24 05:23:16 +0700

    Обновление библиотек

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.bignerdranch.android.photogallery"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.bignerdranch.android.photogallery"
