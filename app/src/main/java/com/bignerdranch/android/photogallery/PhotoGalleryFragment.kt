018882d1d20e2624ad0a8281e3853b011da19c42
hai-mark <errrorfour@gmail.com>
2024-12-24 05:06:41 +0700

    Использование FlickrFetchr в  PhotoGalleryFragment (PhotoGalleryFragment.kt)

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -1,15 +1,22 @@
 package com.bignerdranch.android.photogallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
+private const val TAG = "PhotoGalleryFragment"
+
 class PhotoGalleryFragment : Fragment() {
+
     private lateinit var photoRecyclerView : RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,6 +27,18 @@ class PhotoGalleryFragment : Fragment() {
         photoRecyclerView.layoutManager = GridLayoutManager(context, 3)
         return view
     }
+
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        val flickrLiveData: LiveData<String> =
+            FlickrFetchr().fetchContents()
+        flickrLiveData.observe(
+            this,
+            Observer { responseString ->
+                Log.d(TAG, "Response received: $responseString")
+            })
+    }
+
     companion object {
         fun newInstance() = PhotoGalleryFragment()
     }
