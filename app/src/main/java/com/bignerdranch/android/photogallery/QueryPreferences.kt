e1b141d0bf3073fb4f0ca435174257b4297e116d
hai-mark <errrorfour@gmail.com>
2024-12-24 06:53:11 +0700

    Использование Android KTX  (QueryPreferences.kt)

--- app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.android.photogallery
 
 import android.content.Context
 import android.preference.PreferenceManager
+import androidx.core.content.edit
 
 private const val PREF_SEARCH_QUERY = "searchQuery"
 object QueryPreferences {
@@ -9,11 +10,11 @@ object QueryPreferences {
         val prefs = PreferenceManager.getDefaultSharedPreferences(context)
         return prefs.getString(PREF_SEARCH_QUERY, "")!!
     }
-
+    //
     fun setStoredQuery(context: Context, query: String) {
         PreferenceManager.getDefaultSharedPreferences(context)
-            .edit()
-            .putString(PREF_SEARCH_QUERY, query)
-            .apply()
+            .edit {
+                putString(PREF_SEARCH_QUERY, query)
+            }
     }
 }
